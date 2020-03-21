b0176fe3093c83cfef77c067e9c65943b3748696
Casey Flynn <cflynn.us@gmail.com>
2020-03-21 20:42:28 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader' // eslint-disable-line no-unused-vars
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <h1> Hello, World! Casey Here.</h1>
+      </div>
+    )
+  }
+}
+
+export default App
